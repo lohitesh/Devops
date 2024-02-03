45fc532f526d17476cb9cfcf09667253cdb73021
lohitesh <50686873+lohitesh@users.noreply.github.com>
2024-02-03 16:26:19 +0530

    Update upload.py

--- Python/upload.py
+++ Python/upload.py
@@ -1,3 +1,11 @@
+import requests
+local_file_path = "name of the file to upload"
+upload_url = "Jfrog url to upload"
+headers = {
+        "X-JFrog-Art-Api": jfrog_api_key (token in jfrog)
+    }
+
+
 with open(local_file_path, "rb") as file:
     response = requests.put(upload_url, data=file, headers=self.headers)
 
