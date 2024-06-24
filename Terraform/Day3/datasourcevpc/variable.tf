10265c63a174af8ec39be1fb19f822e8696d82e8
lohitesh <50686873+lohitesh@users.noreply.github.com>
2024-06-24 16:34:50 +0530

    Create variable.tf

---
+++ Terraform/Day3/datasourcevpc/variable.tf
@@ -0,0 +1,12 @@
+variable ami {
+        description = "ami of the iserver"
+}
+
+variable instance_type {
+        description = "instance type of server"
+}
+
+variable aws_region {
+        description = "Region where we need to create"
+        default = "us-east-2"
+}
