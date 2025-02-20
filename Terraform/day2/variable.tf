973447432362d6967689ceeefd963ef553f8f92a
lohitesh <50686873+lohitesh@users.noreply.github.com>
2025-02-20 17:52:27 +0530

    Update variable.tf

--- Terraform/day2/variable.tf
+++ Terraform/day2/variable.tf
@@ -2,7 +2,7 @@ output "instance_public_ip" {
   description = "The public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
-root@ip-172-31-34-156:/home/ubuntu/day1# cat variable.tf 
+
 variable ami {
         description = "ami of the iserver"
 }
