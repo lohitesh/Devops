0b248382271dd50282c5c720956ef33576f89e19
lohitesh <50686873+lohitesh@users.noreply.github.com>
2026-04-26 06:40:34 +0530

    Update ignore_changes.tf

--- Terraform/Day3/lifecycle/ignore_changes.tf
+++ Terraform/Day3/lifecycle/ignore_changes.tf
@@ -8,3 +8,19 @@ lifecycle {
 
 }
 }
+#to explain step by step execute below commands
+# terraform apply .... once u execute apply it will create file with content we love devops class111
+# now change the content of the file via terraform scripts observe below code
+resource "local_file" "pet1" {
+ filename = "/home/ubuntu/test1/pet1.txt"
+ content = "we love Devops class111  Manuall editing"
+lifecycle {
+        ignore_changes = [
+                content
+                ]
+
+}
+}
+
+#now execute terraform apply ... it will now allow the changes to modifythe content since we have used ignore_changes content
+# it will ignore the changes done
